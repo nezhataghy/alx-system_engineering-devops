4529d379909d056ea095b1c66e2c3ce6ec69f6c8
nezhataghy <nezhataghy@gmail.com>
2024-01-02 00:45:13 +0000

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# This manifest configures ssh config file
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => "   PasswordAuthentication no",
+  ensure => present,
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => "   IdentityFile ~/.ssh/school",
+  ensure => present,
+}
