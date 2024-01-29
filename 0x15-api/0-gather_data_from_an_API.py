d4e7f3dd9c597090342884954ed22c45cfee01b6
nezhataghy <nezhataghy@gmail.com>
2024-01-29 21:19:51 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""Gather data from an API"""
+
+import requests
+import sys
+
+
+def tasks_done(id):
+
+    url = "https://jsonplaceholder.typicode.com/"
+    json_reponse = requests.get(url + "users/{}".format(id)).json()
+    name_of_emp = json_reponse.get("name")
+    json_todos = requests.get(url + "users/{}/todos".format(id)).json()
+    total_tasks = len(json_todos)
+
+    task_completed = 0
+    task_list = ""
+
+    for task in json_todos:
+        if task.get("completed") is True:
+            task_completed += 1
+            task_list += "\t " + task.get("title") + "\n"
+
+    print("Employee {} is done with tasks({}/{}):".format(name_of_emp,
+                                                          task_completed,
+                                                          total_tasks))
+    print(task_list[:-1])
+
+
+if __name__ == "__main__":
+    tasks_done(sys.argv[1])
