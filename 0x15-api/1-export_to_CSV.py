cbcdb81cbc66c053f1dd4327dd8601e26808ac28
nezhataghy <nezhataghy@gmail.com>
2024-01-29 22:08:15 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Export to CSV"""
+import requests
+import sys
+
+
+def TODO_progress(id):
+
+    url = "https://jsonplaceholder.typicode.com/"
+    json_reponse = requests.get(url + "users/{}".format(id)).json()
+    name_of_emp = json_reponse.get("name")
+    json_todos = requests.get(url + "users/{}/todos".format(id)).json()
+
+    file_name = "{}.csv".format(id)
+    with open(file_name, "a") as fd:
+        for task in json_todos:
+            completed = task.get("completed")
+            title = task.get("title")
+            csv_data = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(id,
+                                                              name_of_emp,
+                                                              completed,
+                                                              title
+                                                              )
+            fd.write(csv_data)
+
+
+if __name__ == "__main__":
+    TODO_progress(sys.argv[1])
