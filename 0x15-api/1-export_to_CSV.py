a94e880c662e5a997f33fcc7c3dcf89af33d2f56
nezhataghy <nezhataghy@gmail.com>
2024-01-29 23:26:33 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,15 +8,15 @@ import sys
 def TODO_progress(id):
     user_id = id
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    json_reponse = requests.get(url + "users/{}".format(user_id)).json()
+    username = json_reponse.get("username")
+    json_todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+        ) for t in json_todos]
 
 
 if __name__ == "__main__":
