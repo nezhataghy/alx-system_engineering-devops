74c7b36e89d363719843ba6e59491d6b4945883c
nezhataghy <nezhataghy@gmail.com>
2024-01-30 00:04:35 +0000

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,13 +10,13 @@ def TODO_progress():
 
     with open("todo_all_employees.json", "w") as file:
         json.dump({
-            user.get("id"): [{
+            usr.get("id"): [{
                 "task": rep.get("title"),
                 "completed": rep.get("completed"),
-                "username": user.get("username")
+                "username": usr.get("username")
             } for rep in requests.get(url + "todos",
-                                      params={"userId": user.get("id")}).json()]
-            for user in users}, file)
+                                      params={"userId": usr.get("id")}).json()]
+            for usr in users}, file)
 
 
 if __name__ == "__main__":
