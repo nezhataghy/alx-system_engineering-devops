2496cbbcbc8c7556ec6fadab1cc1654d17971d87
nezhataghy <nezhataghy@gmail.com>
2024-02-13 21:35:21 +0000

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Sending a request to reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """Function send a request to reddit API"""
+
+    headers = {'User-Agent': 'MyApp/4.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    data = requests.get(url=url, headers=headers, allow_redirects=False)
+    if data.status_code == 200:
+        hot_ten = data.json().get('data').get('children')
+        if not hot_ten:
+            return print("None")
+        else:
+            for child in hot_ten:
+                print(child.get('data').get("title"))
+    else:
+        return print("None")
